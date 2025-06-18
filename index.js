3be0d7a24a7a39a419afa8fc581fb42f5fab95a2
RatnaTeja9 <ratnateja.ds@gmail.com>
2025-06-18 15:52:53 +0530

    adding style :

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const app = express();
 const port = 3000;
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/view"));
-app.set();
+app.use(express.static(path.join(__dirname, "public")))
+
 
 app.use((req, res, next) => {
   console.log("this is Middleware ");
